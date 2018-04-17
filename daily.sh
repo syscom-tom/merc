12f92b3c101e3a763caeb9d5a728b1792e14fdb2
Tom <tom.syscom@gmail.com>
2018-04-17 22:30:57 +0800

    check adb root Permission. update stop merc method.

--- daily.sh
+++ daily.sh
@@ -10,18 +10,20 @@ source "$HOST_MERC_DIR/var.list";
 
 # perpare 
 $ADB_CMD connect 192.168.122.48:5555
-$ADB_CMD root # need check success
-$ADB_CMD connect 192.168.122.48:5555
+while [ `$ADB_CMD shell "id|cut -d '(' -f1"` = "uid=0" ]; do
+	$ADB_CMD root # need check success
+	$ADB_CMD connect 192.168.122.48:5555
+done;
 
 ### for guild members
 echo "=== guild member sign in ===";
 for I in `echo $GUILD_MEMBERS`; do 
 	echo "$(date +%Y/%m/%d\ %H:%M) $I";
 	if [ ! -f "$HOST_MERC_DIR/account/$I" ]; then echo "$HOST_MERC_DIR/account/$I not exist. please check member list."; exit 1; fi; # check parameters
-	$ADB_CMD push "$HOST_MERC_DIR/account/$I" /data/data/jp.co.happyelements.toto/shared_prefs/jp.co.happyelements.toto.v2.playerprefs.xml 1>/dev/null
+	$ADB_CMD push "$HOST_MERC_DIR/account/$I" /data/data/jp.co.happyelements.toto/shared_prefs/jp.co.happyelements.toto.v2.playerprefs.xml 1>/dev/null;
 # sign in
 	$ADB_CMD shell input tap 550 300 # tap merc icon
-	$ADB_CMD shell 'for i in `seq 1 1 20`; do input tap 1200 700; done'
+	$ADB_CMD shell 'for i in `seq 1 1 22`; do input tap 1200 700; done'
 	$ADB_CMD shell input tap 1150 750 # button_log
 	#for n in `seq 1 1 50`; do $ADB_CMD shell input tap 800 50; done; # button_log_救援
 	$ADB_CMD shell input tap 800 50 # button_log_救援
@@ -38,16 +40,16 @@ for I in `echo $GUILD_MEMBERS`; do
 	$ADB_CMD shell input tap 800 600 # button_Y/N-Y
 	$ADB_CMD shell input tap 600 600 # button_OK
 	$ADB_CMD shell input tap 600 600 # button_OK
-	$ADB_CMD shell "sh /data/data/merc/stop.sh" # stop merc
+	$ADB_CMD shell "ps|grep happyelement|cut -d ' ' -f5|xargs kill" # stop merc 
 done;
 ### for all account
 echo "=== all account sign in ==="; 
 for I in `ls $HOST_MERC_DIR/account/`; do 
 	echo "$(date +%Y/%m/%d\ %H:%M) $I";
-	$ADB_CMD push "$HOST_MERC_DIR/account/$I" /data/data/jp.co.happyelements.toto/shared_prefs/jp.co.happyelements.toto.v2.playerprefs.xml 1>/dev/null
+	$ADB_CMD push "$HOST_MERC_DIR/account/$I" /data/data/jp.co.happyelements.toto/shared_prefs/jp.co.happyelements.toto.v2.playerprefs.xml 1>/dev/null;
 # sign in
 	$ADB_CMD shell input tap 550 300 # tap merc icon
-	$ADB_CMD shell 'for i in `seq 1 1 20`; do input tap 1200 700; done'
+	$ADB_CMD shell 'for i in `seq 1 1 22`; do input tap 1200 700; done'
 	$ADB_CMD shell input tap 1150 750 # button_log
 	#for n in `seq 1 1 50`; do $ADB_CMD shell input tap 800 50; done; # button_log_救援
 	$ADB_CMD shell input tap 800 50 # button_log_救援
@@ -72,9 +74,8 @@ for I in `ls $HOST_MERC_DIR/account/`; do
 	$ADB_CMD shell input tap 500 50 # button_accept_gift
 	$ADB_CMD shell input tap 800 600 # button_Y/N-Y
 	$ADB_CMD shell input tap 600 600 # button_OK
-	$ADB_CMD shell "sh /data/data/merc/stop.sh" # stop merc
+	$ADB_CMD shell "ps|grep happyelement|cut -d ' ' -f5|xargs kill" # stop merc 
 done;
 $ADB_CMD shell poweroff;
-#echo 'shutdown'|at now + 3 minutes;
 
 
